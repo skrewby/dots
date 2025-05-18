759b08e03e95013ceb6493ef3a9b1462320b0dd3
skrewby <33297928+skrewby@users.noreply.github.com>
2025-05-18 20:02:38 +1000

    Correctly select colorscheme in nvim

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -7,12 +7,6 @@ require("lazy").setup({
 	-- keys can be used to configure plugin behavior/loading/etc.
 	--
 	-- Use `opts = {}` to force a plugin to be loaded.
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "catppuccin",
-		},
-	},
 
 	-- Modular approach: using `require 'path/name'` will
 	-- include a plugin definition from file lua/path/name.lua
@@ -37,6 +31,8 @@ require("lazy").setup({
 	require("plugins/treesitter"),
 	require("plugins/which-key"),
 	require("plugins/yazi"),
+
+	install = { colorscheme = { "catppuccin" } },
 }, {
 	ui = {
 		-- If you are using a Nerd Font: set icons to an empty table which will use the
